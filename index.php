d2c466026c5099d2ddb907e27f9b01c00e35f6c6
Giacomo Rizzi <gufoes@gmail.com>
2025-02-10 13:24:14 +0100

    update example to run the coverage report twice

--- index.php
+++ index.php
@@ -3,28 +3,38 @@ require_once 'vendor/autoload.php';
 require_once 'lib.php';
 
 use SebastianBergmann\CodeCoverage\CodeCoverage;
-use SebastianBergmann\CodeCoverage\Data\RawCodeCoverageData;
 use SebastianBergmann\CodeCoverage\Filter;
 use SebastianBergmann\CodeCoverage\Driver\Selector;
 
+function report_code()
+{
+    // If no coverage stored yet, create a new one
+    $filter = new Filter();
+    $filter->includeFile('lib.php');
 
-// If no coverage stored yet, create a new one
-$filter = new Filter();
-$filter->includeFile('lib.php');
+    // Create a new coverage object
+    $coverage = new CodeCoverage(
+        (new Selector)->forLineCoverage($filter),
+        $filter
+    );
 
-$coverage = new CodeCoverage(
-    (new Selector)->forLineCoverage($filter),
-    $filter
-);
+    // Start coverage
+    $coverage->start('xx');
 
-$coverage->start('xx');
-RandomClass::method1();
-$data = $coverage->stop();
+    // Execute the code to be tested
+    RandomClass::method1();
 
-$coverage->append($data, 'session');
+    // Stop and generate report
+    $coverage->stop();
+
+    // Generate the final report
+    $report = new \SebastianBergmann\CodeCoverage\Report\Html\Facade();
+    $report->process($coverage, 'report');
+}
 
 
+// Run a coverage report
+report_code();
 
-// Generate the final report
-$report = new \SebastianBergmann\CodeCoverage\Report\Html\Facade();
-$report->process($coverage, 'report');
+// Run a coverage report again: this will be broken
+// report_code();
